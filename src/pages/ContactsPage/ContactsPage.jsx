7b7b44e2bb2334a17166348d16840d127556f236
serg444s <143083342+serg444s@users.noreply.github.com>
2024-05-06 19:18:57 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,18 +2,19 @@ import { useDispatch, useSelector } from "react-redux";
 import DocumentTitle from "../../DocumentTitle";
 import { selectIsLoading } from "../../redux/contacts/selectors";
 import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contacts/operations";
 import ContactList from "../../components/ContactList/ContactList";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import { Toaster } from "react-hot-toast";
 import Loader from "../../components/Loader/Loader";
+import { fetchContacts } from "../../redux/contacts/operations";
+
 const ContactsPage = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
-    dispatch(fetchContacts);
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
